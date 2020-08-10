a063e3a6a6b62de9671c59f2a63d78138ab24f50
Lucimara Sabino <lucimara1407@gmail.com>
2020-08-10 15:28:08 +0100

    Aulas 1, 2 e 3
    Projeto desenvolvido com o apoio da Rocketseat na Next Level Week (NLW)
    Um site totalmente desenvolvido para o aprendizado

--- src/server.js
+++
@@ -1,9 +0,0 @@
-/*require('express')()
-.get("/", (req, res)=> {
-    return res.sendFile(__dirname + "/views/index.htm)
-})
-.get("/study", (req,res) => {
-    return res.send("Study")
-})
-.listen(5500)*/
-console.log(__dirname + "/views/index.html")
