24b8a8d53d4dc53670cd43dc65e2855f86ad6b4e
Lucimara Sabino <lucimara1407@gmail.com>
2020-08-18 21:16:26 +0100

    5.1

--- src/database/createProffy.js
+++ src/database/createProffy.js
@@ -1,16 +1,16 @@
-module.exports = async function (db, {proffyValue, classValue, classScheduleValue}) {
-    //inserir dados table de teachers
+module.exports = async function(db, { proffyValue, classValue, classScheduleValues }) {
+    //inserir dados table de proffys
     const insertedProffy = await db.run(`
         INSERT INTO proffys (
             name,
             avatar,
             whatsapp,
             bio
-        ) VALUES(
-            ${proffyValue.name},
-            ${proffyValue.avatar},
-            ${proffyValue.whatsapp},
-            ${proffyValue.bio}
+        ) VALUES (
+            "${proffyValue.name}",
+            "${proffyValue.avatar}",
+            "${proffyValue.whatsapp}",
+            "${proffyValue.bio}"
         );
     `)
 
@@ -23,29 +23,32 @@ module.exports = async function (db, {proffyValue, classValue, classScheduleValu
                cost,
                proffy_id 
             ) VALUES (
-                ${classValue.subject},
-                ${classValue.cost},
-                ${proffy_id}
+                "${classValue.subject}",
+                "${classValue.cost}",
+                "${proffy_id}"
             );
     `) 
 
-    const class_id = insertedProffy.lastID
+    const class_id = insertedClass.lastID
 
     //inserir dados na tabela class_schedule
-    const insertedAllclassScheduleValues = classScheduleValues.map(() => {
+    const insertedAllClassScheduleValues = classScheduleValues.map((classScheduleValue) => {
         return db.run(`
-            INSERT INTO class_schedule(
+            INSERT INTO class_schedule (
                 class_id,
                 weekday,
                 time_from,
                 time_to
             ) VALUES (
-
-                
+                "${class_id}",
+                "${classScheduleValue.weeday}",
+                "${classScheduleValue.time_from}",
+                "${classScheduleValue.time_to}"
             );
         `)
     })
 
 
     //aqui vou executar todos os db.runs() das class_schudules
+    await Promise.all(insertedAllClassScheduleValues)
 }
--- src/database/test.js
+++ src/database/test.js
@@ -1,37 +1,41 @@
-const db = require('./db')
+const Database = require('./db')
+const createProffy = require('./createProffy')
 
-db.then((db) => {
+
+Database.then(async (db) => {
     //inserir dados
-
-    proffy = {
-    name: "Diego Fernandes", 
-    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-    whatsapp: "988227681",     
-    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    proffyValue = {
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "988227681",     
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
     }
 
-classvalue = {
-    suject: "Química", 
-    cost: "20", 
-    //o proffy id virá pelo banco de dados
-}
+    classValue = {
+        suject: "Química", 
+        cost: "20", 
+        //o proffy id virá pelo banco de dados
+    }
 
-classSchedule = [
+    classScheduleValues = [
     //class_id virá pelo banco de dados, após cadastrarmos a class
     {
         weekday: 1,
         time_from: 720, 
         time_to: 1220
     },
-
     {
         weekday: 0,
         time_from: 520, 
         time_to: 1220
     }
-]
+    ]
     
-    //createProffy(db, {proffyValue, calssValue, classScheduleValue})
+    //await createProffy(db, {proffyValue, classValue, classScheduleValues})
 
     //consultar os dados inseridos
+
+    //todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    console.log(selectedProffys) 
 })
